308e1162b4cbec121dc93ae76dd540c2418f965e
John Davison <johncdavison@gmail.com>
2014-08-06 23:15:54 -0700

    Add todo's, edit readme.

--- app.rb
+++ app.rb
@@ -1,2 +1,17 @@
 require 'sinatra'
 require 'json'
+
+get("/") do
+  erb :home
+end
+
+get("/say_hello") do
+  puts "\n"
+  p params
+  puts "\n"
+  if params[:message]
+    "<h3>#{params[:message]}</h3>" * 10
+  else
+    "you forgot the message :)"
+  end
+end
